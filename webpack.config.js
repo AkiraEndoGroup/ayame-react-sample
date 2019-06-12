94ce9ee85cda1cb10386b596c655f3c93492d67e
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2019-06-12 23:29:01 +0900

    webpack-nano にする

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
 const path = require("path");
 const webpack = require("webpack");
 const htmlWebpackPlugin = require("html-webpack-plugin");
+const { WebpackPluginServe } = require('webpack-plugin-serve');
+const watch = process.env.NODE_ENV === 'development';
 module.exports = (env, argv) => {
   const mode = process.env.NODE_ENV || "development";
   const isProduction = mode === "production";
+  const watch = mode === 'development';
 return {
     mode: mode,
+    devtool: 'cheap-eval-source-map',
     entry: {
-      main: [path.resolve(__dirname, "src/index.tsx")]
+      main: [
+        path.resolve(__dirname, "src/index.tsx"),
+        'webpack-plugin-serve/client'
+      ]
     },
     output: {
-      filename: isProduction ? "bundle.[chunkhash].js" : "[name].js",
+      filename: isProduction ? "bundle.[hash].js" : "[name].js",
       path: path.resolve(__dirname, "dist")
     },
     devtool: isProduction ? false : "source-map",
@@ -32,7 +39,15 @@ return {
     plugins: [
       new htmlWebpackPlugin({
         template: path.resolve(__dirname, "src/index.html"),
+      }),
+      new WebpackPluginServe({
+        hmr: true,
+        historyFallback: true,
+        static: [path.resolve(__dirname, "dist")],
+        host: "localhost",
+        port: 8080
       })
-    ]
+    ],
+    watch
   }
 }
